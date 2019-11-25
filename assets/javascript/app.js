e19a30f4f82ada090fa6ed54945c111098bf4370
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-11-25 13:28:55 -0800

    pushing to database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,10 +19,20 @@ $(document).ready(function(){
    
    
     $("#add-train").on("click", function() {
+       
         event.preventDefault();
         name = $("#train-name").val().trim();
         destination = $("#destination").val().trim();
         firstTrain = $("#first-train").val().trim();
         frequency = $("#frequency").val().trim();
+       
+       database.ref().push({
+            name: name,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        $("form")[0].reset();
     });
 }
