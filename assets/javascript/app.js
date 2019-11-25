192610b7b318364894cfc6aa64b49608c06761e5
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-11-25 12:33:05 -0800

    Initialize firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,14 @@
 $(document).ready(function(){
+   var config = {
+        apiKey: "AIzaSyBPCAxjycmuYaeY8RISjpPzUOSfmZIbQrQ",
+        authDomain: "train-scheduler-68643.firebaseapp.com",
+        databaseURL: "https://train-scheduler-68643.firebaseio.com",
+        projectId: "train-scheduler-68643",
+        storageBucket: "train-scheduler-68643.appspot.com",
+        messagingSenderId: "934455543386",
+        appId: "1:685154393980:web:3005c4b45483267fdd4ef9"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
 }
